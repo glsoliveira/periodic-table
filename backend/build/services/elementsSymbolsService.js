2f52d7d12ca6ee0b908f14a4935db026f0aa92d8
Gabriel Oliveira <gabriel.leal.s.oliveira@gmail.com>
2024-04-15 18:11:39 -0400

    Delete build folder from backend

--- backend/build/services/elementsSymbolsService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getElements = void 0;
-const elementsSymbolsRepository_1 = require("../repositories/elementsSymbolsRepository");
-const getElements = () => {
-    return (0, elementsSymbolsRepository_1.fetchAllElements)();
-};
-exports.getElements = getElements;
-//# sourceMappingURL=elementsSymbolsService.js.map
--- backend/build/tests/setup/setupTests.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=setupTests.js.map
